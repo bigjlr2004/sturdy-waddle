277b7f34a5695fb8ffbe614d7f3907de7150541c
John Roberts <bigjlr2004@yahoo.com>
2023-03-09 07:09:31 -0600

    Phase 4 Complete

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,55 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
-    {
 
+    //1) Create a list of Smack talking quotes,
+    //2) Create a random number to choose a quote from the index of quotes.
+    //3) Should be a random integer getter from the Play method on Program we can use.
+    //4) Copy the Play from smack talker and paste it into CreativeSmackTalkingPlayer
+    //5) Change Player one to be a CreativeSmackTalkingPlayer.
+    public class CreativeSmackTalkingPlayer : Player
+    {
+        //1) Create a list of Smack talking quotes,
+        public List<string> smacks = new List<string>()
+        {
+        "Here comes the heat!",
+        "These dice are feeling awful lucky. For me of course!!",
+        "You hear that? Thats the sound of these dice heating up.",
+        "I can barely handle these dice they are so hot."
+
+        };
+
+
+
+        public override void Play(Player other)
+        {
+            // Call roll for "this" object and for the "other" object
+            int myRoll = Roll();
+            int otherRoll = other.Roll();
+            //Creates a Random Number
+            Random randomNumberGenerator = new Random();
+            string Taunt = smacks[randomNumberGenerator.Next(0, smacks.Count)];
+            Console.WriteLine($"{Name} Says {Taunt}");
+            Console.WriteLine($"{Name} rolls a {myRoll}");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            if (myRoll > otherRoll)
+            {
+
+                Console.WriteLine($"{Name} Wins!");
+            }
+            else if (myRoll < otherRoll)
+            {
+                Console.WriteLine($"{other.Name} Wins!");
+            }
+            else
+            {
+                // if the rolls are equal it's a tie
+
+                Console.WriteLine("It's a tie");
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ namespace ShootingDice
     {
         static void Main(string[] args)
         {
-            Player player1 = new SmackTalkingPlayer("You can't Beat me.");
+            // Player player1 = new SmackTalkingPlayer("You can't Beat me.");
+            // player1.Name = "Bob";
+
+            Player player1 = new CreativeSmackTalkingPlayer();
             player1.Name = "Bob";
 
             Player player2 = new Player();
