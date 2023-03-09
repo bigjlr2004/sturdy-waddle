2fa0618a220be0ff565708f85b20c0bd5a93213f
John Roberts <bigjlr2004@yahoo.com>
2023-03-09 05:28:39 -0600

    Phase 1 Complete

--- Program.cs
+++ Program.cs
@@ -8,13 +8,14 @@ namespace ShootingDice
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player();
+            SmackTalkingPlayer player1 = new SmackTalkingPlayer("You can't Beat me.");
             player1.Name = "Bob";
 
             Player player2 = new Player();
             player2.Name = "Sue";
 
             player2.Play(player1);
+            player1.Play(player2);
 
             Console.WriteLine("-------------------");
 
